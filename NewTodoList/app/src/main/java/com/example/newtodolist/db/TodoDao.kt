16b0e4816cd791cd5f7a9fa6e9ae4e328b269acd
Rege-97 <cogus1027@gmail.com>
2026-01-07 17:08:52 +0900

    study: dao 생성

---
+++ NewTodoList/app/src/main/java/com/example/newtodolist/db/TodoDao.kt
@@ -0,0 +1,21 @@
+package com.example.newtodolist.db
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface TodoDao {
+    @Query("SELECT * FROM TodoEntity")
+    fun findAllTodo(): List<TodoEntity>
+
+    @Insert
+    fun insertTodo(todo: TodoEntity)
+
+    @Update
+    fun updateTodo(todo: TodoEntity)
+
+    @Delete
+    fun deleteTodo(todo: TodoEntity)
+}
