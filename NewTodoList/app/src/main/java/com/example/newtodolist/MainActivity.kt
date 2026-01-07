a7ab6ea802ef581a0b0348ab238d647c005a1baa
Rege-97 <cogus1027@gmail.com>
2026-01-07 18:42:39 +0900

    study: 레이아웃 간격 수정

--- NewTodoList/app/src/main/java/com/example/newtodolist/MainActivity.kt
+++ NewTodoList/app/src/main/java/com/example/newtodolist/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.newtodolist.databinding.ActivityMainBinding
 import com.example.newtodolist.db.AppDatabase
 import com.example.newtodolist.db.TodoDao
@@ -17,6 +18,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var db: AppDatabase
     private lateinit var todoDao: TodoDao
     private lateinit var todoList: ArrayList<TodoEntity>
+    private lateinit var adapter: TodoRecyclerViewAdapter
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,9 +33,34 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        db = AppDatabase.getInstance(this)!!
+        todoDao = db.getTodoDao()
+
+        getAllTodoList()
+
         binding.btnAddTodo.setOnClickListener {
             val intent = Intent(this, ActivityAddTodo::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun getAllTodoList() {
+        Thread { // DB 작업은 백그라운드 스레드에서 작업
+            todoList = ArrayList(todoDao.findAllTodo())
+            setRecyclerView()
+        }.start()
+    }
+    
+    private fun setRecyclerView(){
+        runOnUiThread {
+            adapter = TodoRecyclerViewAdapter(todoList)
+            binding.recyclerView.adapter = adapter
+            binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        }
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        getAllTodoList()
     }
 }
