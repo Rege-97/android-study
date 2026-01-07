3abc5272be791b2b3359f73e7af8ef9171b70f43
Rege-97 <cogus1027@gmail.com>
2026-01-07 15:10:03 +0900

    study: 리사이클 뷰

--- TodoList/app/src/main/java/com/example/todolist/MainActivity.kt
+++ TodoList/app/src/main/java/com/example/todolist/MainActivity.kt
@@ -1,20 +1,67 @@
 package com.example.todolist
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.todolist.databinding.ActivityMainBinding
+import com.example.todolist.db.AppDatabase
+import com.example.todolist.db.TodoDao
+import com.example.todolist.db.TodoEntity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding   // 뷰에 바로 접근 가능
+
+    private lateinit var db: AppDatabase
+    private lateinit var todoDao: TodoDao
+    private lateinit var todoList: ArrayList<TodoEntity>
+    private lateinit var adapter: TodoRecyclerViewAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        db = AppDatabase.getInstance(this)!!
+        todoDao = db.getTodoDao()
+
+        getAllTodoList()
+
+        binding.btnAddTodo.setOnClickListener {
+            val intent = Intent(this, AddTodoActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun getAllTodoList() {
+        Thread { // DB 작업은 백그라운드 스레드에서 작업
+            todoList = ArrayList(todoDao.getAllTodo())
+            setRecyclerView()
+        }.start()
+    }
+
+    private fun setRecyclerView() {
+        runOnUiThread {
+            adapter = TodoRecyclerViewAdapter(todoList)
+            binding.recyclerview.adapter = adapter
+            binding.recyclerview.layoutManager = LinearLayoutManager(this)
+        }
+    }
+
+    // 메인 액티비디를 다시 불러올 때 리스트도 갱신하기 위해 구현
+    override fun onRestart() {
+        super.onRestart()
+        getAllTodoList()
     }
 }
---
+++ TodoList/app/src/main/java/com/example/todolist/TodoRecyclerViewAdapter.kt
@@ -0,0 +1,63 @@
+package com.example.todolist
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.todolist.databinding.ItemTodoBinding
+import com.example.todolist.db.TodoEntity
+
+// RecyclerView 어댑터
+// 데이터 목록을 받아서 화면에 표시
+class TodoRecyclerViewAdapter(private val todoList: ArrayList<TodoEntity>) :
+    RecyclerView.Adapter<TodoRecyclerViewAdapter.MyViewHolder>() {  // RecyclerView.Adapter 상속( 제너릭 타입으로 ViewHolder 클래스 지정 )
+
+    //ViewHolder 클래스
+    //RecyclerView.ViewHolder 상속
+    //아이템 하나의 View들을 보관
+    inner class MyViewHolder(binding: ItemTodoBinding) : RecyclerView.ViewHolder(binding.root) {
+        val tv_importance = binding.tvImportance
+        val tv_title = binding.tvTitle
+
+        val root = binding.root
+    }
+
+    // 아이템 View 생성
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): MyViewHolder {
+        val binding: ItemTodoBinding =
+            ItemTodoBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MyViewHolder(binding)
+    }
+
+    // 데이터를 View에 바인딩
+    override fun onBindViewHolder(
+        holder: MyViewHolder,
+        position: Int
+    ) {
+        val todoData = todoList[position]   // position 번째 TodoEntity 가져오기
+        when (todoData.importance) {
+            1 -> {
+                holder.tv_importance.setBackgroundColor(R.color.red)
+            }
+
+            2 -> {
+                holder.tv_importance.setBackgroundColor(R.color.yellow)
+            }
+
+            3 -> {
+                holder.tv_importance.setBackgroundColor(R.color.green)
+            }
+        }
+
+        holder.tv_importance.text = todoData.importance.toString()
+        holder.tv_title.text = todoData.title
+    }
+
+    // RecyclerView에 보여줄 아이템 개수
+    override fun getItemCount(): Int {
+        return todoList.size
+    }
+
+}
